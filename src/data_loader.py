c947eb8563d43fbc741066cd78856e747543a62e
mattgabelli <mgg3478@gmail.com>
2025-11-07 22:52:55 +0000

    Simplified data loader file

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,14 +4,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-ticker_symbol = 'EURUSD=X'
-start_date = '2015-01-01'
-end_date = '2025-01-01'
+ticker_symbol = 'GBPUSD=X'
+start_date = datetime.datetime(2015, 1, 1)
+end_date = datetime.datetime(2025, 1, 1)
 
 def get_log_returns(ticker=ticker_symbol, start=start_date, end=end_date):
     # compute log returns for 10 years of GBP/USD data
-    data = yf.download(ticker, start=start)
-    data['log_return'] = np.log(data['Close'] / data['Close'].shift(1))
-    data.dropna(inplace=True)
-    return data
+    data = yf.download(ticker, start=start, end=end)
+    lr = data.Close.pct_change().dropna()
+    return lr
+
 
